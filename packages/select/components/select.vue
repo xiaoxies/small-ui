00a1d85665f6ea65ddccf798b4c2da45b34b999e
xiechufeng1 <974470833@qq.com>
2020-07-21 18:54:48 +0800

    select组件完成

--- packages/select/components/select.vue
+++ packages/select/components/select.vue
@@ -130,7 +130,6 @@
         },
         methods:{
             clearData(){
-                console.log("进来了吗");
                 if(this.multiple){
                     this.labelArr=[];
                     this.valArr=[];
